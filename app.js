a12dcbfe5419c65063f40181a522de74be7d2dbc
sdinkle <sdinkle88@gmail.com>
2019-08-18 10:36:14 -0400

    Added basic routing (second part of assignment; commented out first part)

--- app.js
+++ app.js
@@ -16,18 +16,29 @@ app.get('/favicon.ico', (req, res, next) => {
     res.status(204).send();
 });
 
-// First part of assignment: Create an Express.js app which funnels the
-// requests through 2 middleware functions that log something to the console
-// and return one response.
-app.use((req, res, next) => {
-    console.log('It\'s the first middleware function! This one passes the control flow onward.');
-    next();
+// // First part of assignment: Create an Express.js app which funnels the
+// // requests through 2 middleware functions that log something to the console
+// // and return one response.
+// app.use((req, res, next) => {
+//     console.log('It\'s the first middleware function! This one passes the control flow onward.');
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log('It\'s the second middleware function! This one returns the response.');
+//     res.send();
+// });
+
+// Second part of assignment: Handle requests to "/" and "/users" such that
+// each request only has one handler/middleware that does something with it
+// (e.g. send dummy response).
+app.use('/users', (req, res, next) => {
+    res.send('<h1>It\'s the "Users" page!');
 });
 
-app.use((req, res, next) => {
-    console.log('It\'s the second middleware function! This one returns the response.');
-    res.send();
-})
+app.use('/', (req, res, next) => {
+    res.send('<h1>It\'s the main page!');
+});
 
 // Begin listening on port 3000.
 app.listen(3000);
